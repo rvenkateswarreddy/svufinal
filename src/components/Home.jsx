c2aa07e127293325b9348c5be92503cbdc32b1be
R.venkateswar reddy <rvenkateswarreddy12345@gmail.com>
2024-03-15 09:33:29 +0530

    animation reveel

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,22 +2,25 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import "./Home.css"; // Import the CSS file
+import "./Home.css";
 
 const Home = () => {
   const notify = () => {
-    alert("please login to access");
+    alert("Please login to access.");
     toast.success("Stay booked successfully!");
   };
 
   return (
     <>
       <ToastContainer />
-
       <div className="hero-section">
-        <h1>SVU HOSTEL</h1>
-        <p>Welcome to SVU Hostel, your home away from home.</p>
+        <h1>
+          <span>Sri</span> <span>Venkateswara</span> <span>University</span>{" "}
+          <span>Hostel</span>
+        </h1>
+        <p className="homingwelcome">
+          Welcome to SVU Hostel, your home away from home.
+        </p>
         <Link to="/login">
           <button className="book-button" onClick={notify}>
             STAY HERE
